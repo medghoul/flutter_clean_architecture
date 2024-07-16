d877d759390a99feed05e66da89e659c0c8cfd52
ghoulhamza <ghoulhamza06@gmail.com>
2024-07-16 23:11:47 +0200

    Update the constants file

--- lib/resources/constants/constants.dart
+++ lib/resources/constants/constants.dart
@@ -2,4 +2,6 @@ class ProjectConst {
   static const NORMAL_TEXTFORMFIELD_MAX_LENGTH = 40;
   static const EMAIL_TEXTFORMFIELD_MAX_LENGTH = 100;
   static const PLACEHOLDER = '##';
+  static const PHONE_NUMBER_TEXTFORMFIELD_MAX_LENGTH = 15;
+  static const FISCAL_CODE_TEXTFORMFIELD_MAX_LENGTH = 16;
 }
